c03ffdcfded05df28f89bbfc93428b10fd8ce91b
Kevin Pagliuca <kevin.pagliuca@outlook.com>
2022-01-14 09:56:18 -0300

    fixings

--- src/shared/infra/server.ts
+++ src/shared/infra/server.ts
@@ -3,5 +3,5 @@ import { app } from './app';
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT || 3333, () =>
-  console.log(`server is running in PORT ${PORT || 3333} 🔥`)
+  console.log(`server is running in PORT ${PORT} 🔥`)
 );
