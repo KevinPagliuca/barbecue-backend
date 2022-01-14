0410a5e28968faca087ca138867c6a08bf84cb86
Kevin Pagliuca <kevin.pagliuca@outlook.com>
2022-01-14 11:02:46 -0300

    fix: fix import paths

--- src/shared/infra/http/routes/churras.routes.ts
+++ src/shared/infra/http/routes/churras.routes.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 
-import { listChurrasByUserController } from '@modules/churras/useCases/listChurrasByUser/ListChurrasByUserController';
-
 import { createChurrasController } from '../../../../modules/churras/useCases/createChurras/CreateChurrasController';
 import { deleteChurrasController } from '../../../../modules/churras/useCases/deleteChurras/DeleteChurrasController';
 import { listChurrasController } from '../../../../modules/churras/useCases/listChurras/ListChurrasController';
 import { listChurrasByIdController } from '../../../../modules/churras/useCases/listChurrasById/ListChurrasByIdController';
+import { listChurrasByUserController } from '../../../../modules/churras/useCases/listChurrasByUser/ListChurrasByUserController';
 import { updateChurrasController } from '../../../../modules/churras/useCases/updateChurras/UpdateChurrasController';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticate';
 
